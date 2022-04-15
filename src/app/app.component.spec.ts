5b4def5f851bfb65bf97d895bcf6f1e14bcc2ffe
William Marques <wmarques.pro@gmail.com>
2022-04-15 18:24:04 +0200

    improve setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,55 @@
-import {render} from "@testing-library/angular";
+import {fireEvent, render, screen} from "@testing-library/angular";
 import {AppComponent} from "./app.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
 
 describe('App Component', () => {
   test('should render', async () => {
-    await render(AppComponent, {
-      imports: [MatToolbarModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule]
+    await renderComponent();
+  });
+
+
+  describe('Email', () => {
+    test('should display invalid error message when email is invalid', async () => {
+      // GIVEN
+      await renderComponent();
+
+
+      // WHEN
+
+      // THEN
+    });
+
+    test('should not display invalid error message when email is valid', async () => {
+      // GIVEN
+      await renderComponent();
+
+      // WHEN
+
+      // THEN
     });
   });
-})
+
+  describe('Global', () => {
+    test('should enable button when everything is valid', async () => {
+      // GIVEN
+      await renderComponent();
+
+      // WHEN
+
+      // THEN
+    })
+  });
+
+  async function renderComponent() {
+    await render(AppComponent, {
+      imports: [MatToolbarModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatCheckboxModule]
+    });
+  }
+});
